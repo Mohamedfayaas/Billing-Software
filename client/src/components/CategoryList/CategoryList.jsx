74901fcf9d8ec7ad709dec63a6a1a3c270833aa4
Mohamed Fayaas <mohamedfayaas211005@gmail.com>
2026-01-21 20:35:41 +0530

    added

--- client/src/components/CategoryList/CategoryList.jsx
+++ client/src/components/CategoryList/CategoryList.jsx
@@ -1,19 +1,38 @@
 import "./CategoryList.css"
-import {useContext} from "react";
+import {useContext, useMemo, useState} from "react";
 import {AppContext} from "../../context/AppContext.jsx";
-import {addCategory} from "../../Service/CategoryService.js";
+// import {addCategory} from "../../Service/CategoryService.js";
 
 const CategoryList = () => {
     const {categories}  = useContext(AppContext);
+    const [searchTerm, setSearchTerm] = useState("");
+
+    const filteredCategories = categories.filter(category =>
+        category.name.toLowerCase().includes(searchTerm.toLowerCase())
+    )
+
     return (
         <div className="category-list-container" style={{height:'100vh',overflowY:'auto',overflowX:'hidden'}}>
             <div className="row pe-2">
-                search bar
+                <div className="input-group mb-3">
+                    <input type="text"
+                           name="keyword"
+                           id="keyword"
+                           placeholder="Search for keyword"
+                           className="form-control"
+                           onChange={(e) => setSearchTerm(e.target.value)}
+                           value={searchTerm}
+                    />
+                    <span className="input-group-text bg-warning">
+                        <i className="bi bi-search">
+                        </i>
+                    </span>
+                </div>
             </div>
             <div className="row g-3 pe-2">
                 {categories.map((category, index) => (
                     <div key={index} className="col-12">
-                        <div className="card p-3" style={{backgroundColor:category.bgcolor}}>
+                        <div className="card p-3" style={{backgroundColor:category.bgColor}}>
                             <div className="d-flex align-items-center">
                                 <div style={{marginRight:'15px'}}>
                                     <img src={category.imgUrl} alt={category.name} className="category-image" />
